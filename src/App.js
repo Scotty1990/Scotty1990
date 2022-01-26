f392af4676c1037333eedd8e1dfaaafd41fccf5a
scottmacleod <t_mac3131@hotmail.com>
2022-01-26 15:37:24 -0600

    finished nav bar and colors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from './components/Modal';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div id="navbar" className='navbar-brand'>
+          <p className='nav-links'><a className='a-tags' href="#top">SCOTT MACLEOD</a></p>
+          <p className='nav-links'><a className='a-tags' href="#portfolio">PORTFOLIO</a></p>
+          <p className='nav-links'><a className='a-tags' href="#about">ABOUT</a></p>
+          <p className='nav-links'><a className='a-tags' href="#contact">CONTACT</a></p>
+        </div>
       </header>
+      <div id="top"></div>
+        <div className="padding-section"></div>
+      <div>
+        <img id="profile-pic" src="https://imgur.com/AK6BmTc.png" alt="scott-macleod" />
+      </div>
+      <div>SCOTT MACLEOD</div>
+      <div id="portfolio"></div>
+      <div className="padding-section"></div>
+      Portfolio
+      <div className='test'>
+
+      </div>
+      <div id="about">
+        <div className="padding-section"></div>
+        About
+      </div>
+      <div className='test'>
+
+      </div>
+      <div id="contact">
+        <div className="padding-section"></div>
+        Contact
+      </div>
+      <div className='test'>
+
+      </div>
     </div>
   );
 }
