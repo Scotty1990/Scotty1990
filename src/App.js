d98591db1bccb6c2831c5cf69f726acad8956f3f
Scott MacLeod <t_mac3131@hotmail.com>
2022-01-29 12:47:37 -0600

    deleted comment

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
   const handleCloseSuperHeroDatabase = () => setShowSuperHeroDatabase(false)
   const handleShowSuperHeroDatabase = () => setShowSuperHeroDatabase(true)
 
-  // need to check if this will get it to update my website
   useEffect(() => {
     document.title = "Scott MacLeod"
   }, [])
